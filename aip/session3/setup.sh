bff682ea933e0685d54406f6039c85d3e1617049
Ubuntu <ubuntu@ip-172-31-30-215.ap-southeast-1.compute.internal>
2025-04-09 07:17:37 +0000

    updates

--- aip/session3/setup.sh
+++ aip/session3/setup.sh
@@ -12,12 +12,12 @@ if [ -d $directory/.env ];
         echo "Directory does not exists."
 
         python -m venv .env
-        source .env/bin/activate
+        source $directory/.env/bin/activate
         pip install -U pip
         pip install -r requirements.txt
         deactivate
 fi
 
-/home/ubuntu/environment/apcr/aip/session3/.env/bin/streamlit run /home/ubuntu/environment/apcr/aip/session3/Home.py --server.port 8081 &
+"$directory"/.env/bin/streamlit run $directory/Home.py --server.port 8081 &
 
 
